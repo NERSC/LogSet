a72072ca29361db860e8411eaeff046e08a2915f
Steve Leak <sleak@lbl.gov>
2019-04-09 16:58:25 -0700

    Start new branch to try a different approach

--- src/util/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from .MultiDict import MultiDict, StringAsValuesWarning
-from .Context import Context
-from . import UI
-from .assorted import ran_str, FileInfo, ParseRule, cast
-
-__all__ = [ 'MultiDict', 'StringAsValuesWarning', 'Context', 'UI', 'ran_str', 
-            'FileInfo', 'ParseRule', 'cast' ]
