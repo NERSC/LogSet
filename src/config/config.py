d3b3a2bd7d43cd275633c7ea7a56fab55bfd8e36
Steve Leak <sleak@lbl.gov>
2019-04-10 22:36:20 -0700

    Add verbosity control

--- src/config/config.py
+++ src/config/config.py
@@ -12,6 +12,9 @@ etc: str = os.path.join(installdir, 'etc')
 import toml
 import deepmerge
 
+# TODO should make config forgiving wrt case sensitiy, eg interpret 'sqlite' as the 
+#      user probably expects
+
 settings = {}
 
 for _path in (os.path.join(etc, "defaults.toml"),
@@ -21,15 +24,40 @@ for _path in (os.path.join(etc, "defaults.toml"),
         with open(_path) as f:
             deepmerge.always_merger.merge(settings, toml.load(f)) 
 
+import logging
+import bidict
+_verbosity_levels = bidict.bidict({
+    'debug': logging.DEBUG,
+    'info': logging.INFO,
+    'warning': logging.WARNING,
+    'error': logging.ERROR,
+    'critical': logging.CRITICAL,
+})
+
 def setup_global_args(parser):
     parser.add_argument('--local-persistence', '-p', dest='dbpath',
         help="path to file or database for local (cached) log metadata",
         default=settings['persistence']['name'])
 
+    parser.add_argument('-v', '--verbose', action='count', default=0,
+        help="Be noisier. -vv is noisier than -v, etc")
+    parser.add_argument('-q', '--quiet', action='count', default=0,
+        help="Be quieter. -qq only shows critical errors")
+
 import typing as t
 
 def update_settings(params: t.Dict[str,str]):
+    print(params)
     global settings
-    if 'dbpath' in params:
-        settings['persistence']['name'] = params['dbpath']
 
+    settings['persistence']['name'] = params['dbpath']
+
+    verbosity_level = _verbosity_levels[settings['verbosity']]
+    verbosity_level = verbosity_level + 10*params['quiet'] - 10*params['verbose']
+    verbosity_level = max(logging.DEBUG, min(verbosity_level, logging.CRITICAL))
+    settings['verbosity'] = _verbosity_levels.inverse[verbosity_level]
+
+def apply_global_config():
+    logging.getLogger().setLevel(_verbosity_levels[settings['verbosity']])
+
+apply_global_config()
