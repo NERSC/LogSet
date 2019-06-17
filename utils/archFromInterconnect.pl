55810bad15ae290e6253024fc3725c50d23cc5b9
Steve Leak <sleak@lbl.gov>
2019-06-17 16:45:51 -0700

    merge in newst version of archFromInterconnect

--- utils/archFromInterconnect.pl
+++ utils/archFromInterconnect.pl
@@ -8,7 +8,7 @@ archFromInterconnect.pl
 
 =head1 SYNOPSIS
 
-cat rtrfile.txt | ./archFromInterconnect.pl > arch.out
+./archFromInterconnect.pl --rtrfile=rtr.out --arch=XC > arch.out
 
 
 =head1 DESCRIPTION
@@ -30,25 +30,43 @@ of rtr --interconnect
 
 =cut
 
+# WARNING: In process of editing for gemini. 
+# Issues:
+# * gemini service dont have same number of nodes as compute. (Do we need to know about service and compute in aries?)
+# * will need to infer the processor facing tiles (l23, l24, l33, l34, l43, l44, l53, l54). Can't recall ptile <-> NIC
+# * torus links are directional, whereas XC links are not. We drop direction here.
 
 use strict;
+use Getopt::Long;
 
+my $fh;
 my $nnic_aries = 4;
+my $nnodeperslot_aries = 4;
 # from the slots, can infer all, cabs, chassis, nodes, aries. Slot->aries is known
 # use hash, so duplicates are automatically handled, even though this means we have increased storage
 my %HoHcabinet; #for each cab, hash of chassis
 my %HoHchassis; # for each chassis, hash of slots
-my %HoHslot; # infer the nodes and the aries
-# TODO: rename so can handle gemini.
-my %HoHaries; # for each aries, the rc will have tiles and NICs.
+my %HoHslot; # for each slot, infer the nodes and the rtrs
+my %HoHaries; # for each aries, tiles and NICs (value is type)
 my %HoHlink; # for each link --  e1, e2, type. name of the link will be name of the tile. includes ptile links
 my %HoHPcieLink; # between node and NIC
-my %HoHendpoint; #for each link endpoint, which it its link? nodes, nice, and tiles all go in here
+my %HoHendpoint; #for each link endpoint, which it its link? nodes, NICs, and tiles all go in here
+#NICs are endpoints of links (from tile) and pcielinks (to node)
 
 #c0-0c0s0a0l00(0:0:0) green -> c0-0c0s6a0l10(0:0:6)
 #c0-0c0s0a0l01(0:0:0) blue -> unused
 #c0-0c0s0a0l50(0:0:0) ptile -> processor
 
+my $arch='';
+my $rtrfile='';
+
+GetOptions("arch=s" => \$arch,
+           "rtrfile=s" => \$rtrfile)
+or die "Error in command line arguments\n";
+
+if (!($arch eq "XC")){ # && !($arch eq "XE")){
+    die "Only XC supported\n";
+}
 
 sub addLink{
     my ($R0,$E0,$R1,$E1,$lname0,$type) = @_;
@@ -61,14 +79,25 @@ sub addLink{
 }
 
 sub addEndpoint{
-    my ($E0,$type,$lname0,$pcie) = @_;
-    $HoHendpoint{$E0}{'type'} = $type;
-    $HoHendpoint{$E0}{'link'} = $lname0;
-    $HoHendpoint{$E0}{'pcielink'} = $pcie;
+    my ($E0,$type,$lname0) = @_;
+    $HoHendpoint{$E0}{'type'} = $type; # ok if this is a repeat
+
+    if ($lname0 eq "N/A"){
+	return; # unused
+    }
+
+    my @arr;
+    if (exists $HoHendpoint{$E0}{'links'} ){
+	@arr =  @{$HoHendpoint{$E0}{'links'}};
+	push @arr, $lname0;
+    } else {
+	$arr[0] = $lname0;
+    }
+    @{$HoHendpoint{$E0}{'links'}} = @arr;
 }
 
 sub printEndpoint{
-    foreach (sort { $b <=> $a } keys(%HoHendpoint) ){
+    foreach (sort { $a <=> $b || $a cmp $b } keys(%HoHendpoint) ){
 	my $ep = $_;
 
 	if ($HoHendpoint{$ep}{'type'} =~ /TLE/){
@@ -79,11 +108,12 @@ sub printEndpoint{
 	    print ":$ep a ddict:ComputeNode ;";
 	}
 
-	if (!($HoHendpoint{$ep}{'link'} =~ /N\/A/)){
-	    print "\n\tlogset:endPointOf :" . $HoHendpoint{$ep}{'link'} . ";";
-	}
-	if (!($HoHendpoint{$ep}{'pcielink'} =~ /N\/A/)){
-	    print "\n\tlogset:endPointOf :" . $HoHendpoint{$ep}{'pcielink'} . ";";
+	if (exists $HoHendpoint{$ep}{'links'}){
+	    foreach my $val ( @{$HoHendpoint{$ep}{'links'}} ){
+		print "\n\tlogset:endPointOf :" . $val . ";";
+	    }
+	} else {
+#	    print "no endpoints for <$ep>\n";
 	}
 	print "\t.\n";
     }    
@@ -91,38 +121,46 @@ sub printEndpoint{
 
 }
 
-sub addAriesTile{
-    my ($R0,$E0) = @_;
-    $HoHaries{$R0}{$E0} = "TLE"; #child of the aries
-}
-
-sub addAriesNIC{
-    my ($R0,$NIC,$nodenum,$lname0) = @_;
-    #adding the NIC, adds the node, and the pcielink
-
-    $HoHaries{$R0}{$NIC} = "NIC"; #child of the aries
-    #add the node
-    if ($R0 =~ /(.*)a0/){
-	my $base = $1;
-	my $node = $base . "n". $nodenum;
-	my $pcie = "pcie" . $node;
-	#node is an enpoint
-	$HoHPcieLink{$pcie} = 1;
-	my @endarr = ();
-	addEndpoint($node,"NDE","N/A",$pcie); #endpoint of the pcie
-	addEndpoint($NIC,"NIC",$lname0,$pcie); #endpoint of the pcie and the ptile link
+sub addTile{
+    my ($R0,$E0,$arch) = @_;
+    if ($arch =~ /XC/){
+	$HoHaries{$R0}{$E0} = "TLE"; #child of the aries
+#    } else {
+#	$HoHgemini{$R0}{$E0} = "TLE"; #child of the gemini DNE yet.....
     }
 }
 
+
+sub addNICandNode{
+    my ($R0,$NIC,$nodenum,$lname0) = @_;
+
+    if (!(exists $HoHaries{$R0}{$NIC})){ 
+	$HoHaries{$R0}{$NIC} = "NIC"; #child of the aries. 
+	#add the node
+	if ($R0 =~ /(.*)a0/){
+	    my $base = $1;
+	    my $node = $base . "n". $nodenum;
+	    my $pcie = "pcie" . $node;
+	    #node is an enpoint
+	    $HoHPcieLink{$pcie} = 1;
+	    my @endarr = ();
+	    
+#	    print "adding endpint <$NIC> to link $pcie\n";
+	    addEndpoint($NIC,"NIC",$pcie); #endpoint of the pcie. this will be unique to the NIC
+#	    print "adding endpint <$node> to link $pcie\n";
+	    addEndpoint($node,"NDE",$pcie); #endpoint of the pcie. this will be unique to the node
+	}
+    }
+#    print "adding endpint <$NIC> to link $lname0\n";
+    addEndpoint($NIC,"NIC",$lname0);     # add this ptile endpoint to the NIC, will be multiples
+}
+
+
 sub printCabinet{
-    foreach (sort { $b <=> $a } keys(%HoHcabinet) ){
-	my $cab = $_;
-    
+    foreach my $cab (sort { $a <=> $b || $a cmp $b } keys(%HoHcabinet) ){
 	print ":$cab a ddict:Cabinet ;\n";
-    print "\trdfs:label \"$cab\" ;\n" ;
-	foreach (sort { $b <=> $a } keys %{ $HoHcabinet{$cab} } ){
-	    my $chassis = $_;
-	    print "\tlogset:hasPart :$chassis ;\n";
+	foreach my $chassis (sort { $a <=> $b || $a cmp $b } keys %{ $HoHcabinet{$cab} } ){
+	    print "\tlogset:hasPart :$chassis;\n";
 	}
 	print "\t.\n";
 	print "\n";
@@ -131,12 +169,11 @@ sub printCabinet{
 
 
 sub printChassis{
-    foreach (sort { $b <=> $a } keys(%HoHchassis) ){
+    foreach (sort { $a <=> $b || $a cmp $b } keys(%HoHchassis) ){
 	my $chassis = $_;
     
 	print ":$chassis a ddict:Chassis ;\n";
-    print "\trdfs:label \"$chassis\" ;\n" ;
-	foreach (sort { $b <=> $a } keys %{ $HoHchassis{$chassis} } ){
+	foreach (sort { $a <=> $b || $a cmp $b } keys %{ $HoHchassis{$chassis} } ){
 	    my $slot = $_;
 	    print "\tlogset:hasPart :$slot;\n";
 	}
@@ -145,18 +182,22 @@ sub printChassis{
     }
 }
 
-sub printSlot{
-    foreach (sort { $b <=> $a } keys(%HoHslot) ){
+sub printAriesSlot{
+
+    my $nnodes = 0;
+    my $rtrtype  = '';
+    my $nnodes = $nnodeperslot_aries;
+
+    foreach (sort { $a <=> $b || $a cmp $b } keys(%HoHslot) ){
 	my $slot = $_;
 	print ":$slot a ddict:Blade ;\n";
-    print "\trdfs:label \"$slot\" ;\n" ;
 
 	#well known slots have nodes
-	for (my $i = 0; $i < 4; $i++){
+	for (my $i = 0; $i < $nnodes; $i++){
 	    print "\tlogset:hasPart :$slot" . "n$i;\n";
 	}
 
-	#well known slots have aries
+	#well known slots have routers
 	print "\tlogset:hasPart :$slot" . "a0;\n";
 	print "\t.\n";
 	print "\n";
@@ -167,13 +208,12 @@ sub printSlot{
 sub printAries{
     #aries will have both tile and NIC children. they will be added as endpoints
 
-    foreach (sort { $b <=> $a } keys(%HoHaries) ){
+    foreach (sort { $a <=> $b || $a cmp $b } keys(%HoHaries) ){
 	my $aries = $_;
 	print ":$aries a craydict:AriesRouter ;\n";
-    print "\trdfs:label \"$aries\" ;\n" ;
-	foreach (sort { $b <=> $a } keys %{ $HoHaries{$aries} } ){
+	foreach (sort { $a <=> $b || $a cmp $b } keys %{ $HoHaries{$aries} } ){
 	    my $child = $_; # children are both TILES and NICS. Here we do not care which type
-	    print "\tlogset:hasPart :$child ;\n";
+	    print "\tlogset:hasPart :$child;\n";
 	}
 	print "\t.\n";
 	print "\n";
@@ -181,16 +221,16 @@ sub printAries{
 }
 
 sub printPcieLink{
-    foreach (sort { $b <=> $a } keys(%HoHPcieLink) ){
+    foreach (sort { $a <=> $b || $a cmp $b } keys(%HoHPcieLink) ){
 	my $link = $_;
-	print ":$link a ddict:pcieLink .\n";
+	print ":$link a ddict:PCIeLink .\n";
     }
     print "\n";
 }
     
 
 sub printLink{
-    foreach (sort { $b <=> $a } keys(%HoHlink) ){
+    foreach (sort { $a <=> $b || $a cmp $b } keys(%HoHlink) ){
 	my $link = $_;
 
 	if ($HoHlink{$link}{'type'} =~ /BLU/){
@@ -200,45 +240,40 @@ sub printLink{
 	} elsif ($HoHlink{$link}{'type'} =~ /BLK/){
 	    print ":$link a craydict:BlackLink .\n";
 	} elsif ($HoHlink{$link}{'type'} =~ /PTL/){
-	    print ":$link a craydict:Ptile .\n";
+	    print ":$link a craydict:PtileLink .\n";
+	} elsif ($HoHlink{$link}{'type'} =~ /Xp/){
+	    print ":$link a craydict:XpLink .\n";
+	} elsif ($HoHlink{$link}{'type'} =~ /Xm/){
+	    print ":$link a craydict:XmLink .\n";
+	} elsif ($HoHlink{$link}{'type'} =~ /Yp/){
+	    print ":$link a craydict:YpLink .\n";
+	} elsif ($HoHlink{$link}{'type'} =~ /Ym/){
+	    print ":$link a craydict:YmLink .\n";
+	} elsif ($HoHlink{$link}{'type'} =~ /Zp/){
+	    print ":$link a craydict:ZpLink .\n";
+	} elsif ($HoHlink{$link}{'type'} =~ /Zm/){
+	    print ":$link a craydict:ZmLink .\n";
 	}
     }
     print "\n";
 }
 
-sub printPrefixes{
-    print "\@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n";
-    print "\@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n";
-    print "\@prefix adms: <http://www.w3.org/ns/adms#> .\n";
-    print "\@prefix dct: <http://purl.org/dc/terms/> .\n";
-    print "\@prefix logset: <http://portal.nersc.gov/project/mpccc/sleak/resilience/datasets/logset#> .\n";
-    print "\@prefix ddict: <http://portal.nersc.gov/project/mpccc/sleak/resilience/datasets/ddict#> .\n";
-    print "\@base <file:///global/homes/s/sleak/Monitoring/Resilience/LogSet/examples/> .\n";
-    print "\@prefix craydict: <cray-dict#> .\n";
-    print "\n";
-    print "# declare myself and set a prefix:\n";
-    #print "\@base <http://FIXME/where/will/this/be/published> .\n";
-    print "\@base <file:///global/homes/s/sleak/Monitoring/Resilience/LogSet/examples/> .\n";
-    print "\@prefix : <edison-arch#> .\n";
-    print "\n";
-    print ":\n";
-    print "\ta adms:Asset ;\n";
-    print "\tdct:title \"FIXME give this file a title\" ;\n";
-    print "\trdfs:label \"FIXME short label\" ;\n";
-    print "\t.\n";
-    print "\n";
-}
 
-while(<>){
+open(my $fh, "<", $rtrfile)
+    or die "Can't open rtrfile: $!";
+
+while(<$fh>){
     chomp;
     my $line = $_;
     $line =~ tr/-/_/; # WARNING: will need to do underscore in all the matching...
 
     if ($line =~ /\_>/){
 	my @vals = split(/\s+/,$line);
-	if (scalar(@vals) != 4){
-	    die "Bad line: <$line>\n";
+	if (($arch eq "XC" && (scalar(@vals) != 4)) ||
+	    ($arch eq "XE" && (scalar(@vals) != 6))){
+	    die "Bad line <$line>\n";
 	}
+
 	my $E0 = -1;
 	my $E1 = -1;
 	my $R0 = -1;
@@ -252,10 +287,29 @@ while(<>){
 	    $type = "BLK";
 	} elsif ($vals[1] eq 'green'){
 	    $type = "GRE";
-	} elsif ($vals[1] eq 'ptile'){
+	} elsif ($vals[1] eq 'ptile'){ # ptile only in the aries rtr output
 	    $type = "PTL";
 	} elsif ($vals[1] eq 'host'){ # DNE
 	    $type = "HST";
+#NOTE: for gemini direction matters...so either have to double add if tracking directions, or drop +/- if not
+        } elsif ($vals[1] eq 'X+'){
+#	    $type = "Xp";
+	    $type = "X";
+        } elsif ($vals[1] eq 'X_'){
+#	    $type = "Xm";
+	    $type = "X";
+        } elsif ($vals[1] eq 'Y+'){
+#	    $type = "Yp";
+	    $type = "Y";
+        } elsif ($vals[1] eq 'Y_'){
+#	    $type = "Ym";
+	    $type = "Y";
+        } elsif ($vals[1] eq 'Z+'){
+#	    $type = "Zp";
+	    $type = "Z";
+        } elsif ($vals[1] eq 'Z_'){
+#	    $type = "Zm";
+	    $type = "Z";
 	} else {
 	    $type = "UNK";
 	}
@@ -265,7 +319,7 @@ while(<>){
 	    $E0 = $1;
 	    if ($E0 =~ /(.*)l/){
 		$R0 = $1;
-		addAriesTile($R0,$E0);
+		addTile($R0,$E0,$arch);
 	    }
 	}
 
@@ -274,7 +328,7 @@ while(<>){
 	    $E1 = $1;
 	    if ($E1 =~ /(.*)l/){
 		$R1 = $1;
-		#dont add the aries for this side; will pick it up on the other side of the link
+		#dont add the router for this side; will pick it up on the other side of the link
 	    }
 
 	    # these will readin as double, one for each endpoint, so check to see if we already have this one
@@ -282,41 +336,48 @@ while(<>){
 	    my $lname1 = "link" . $E1;
 	    if (!(exists $HoHlink{$lname0}) && !(exists $HoHlink{$lname1})){
 		#add this link, otherwise we already have it from the other end
-		addEndpoint($E0,"TLE",$lname0,"N/A");
-		addEndpoint($E1,"TLE",$lname0,"N/A");
+		#NOTE: for gemini direction matters...so either have to double add if tracking directions, or drop +/- if not
+		addEndpoint($E0,"TLE",$lname0);
+		addEndpoint($E1,"TLE",$lname0);
 		addLink($R0,$E0,$R1,$E1,$lname0,$type);
 	    }
-	} elsif ($vals[3] =~ /unused/){
+	} elsif ($vals[3] =~ /unused/){ # only happens for Aries
 	    #$E1 = "unused"; does not matter
 	    #$R1 = "unused"; does not matter
 	    #add the tile, but not the link
-	    addAriesTile($R0,$E0);
-	    addEndpoint($E0,"TLE","N/A","N/A");
+	    addTile($R0,$E0,$arch);
+	    addEndpoint($E0,"TLE","N/A");
 	} elsif ($vals[3] =~ /processor/){
-	    # TODO: Gemini
-	    # This endpoint is a NIC. aries has 4 NIC and 8 ptiles, in order. get the last digit
-	    my $node = -1;
-	    if ($E0 =~ /.*(\d)/){
-		my $lname0 = "link" . $E0;
-		$R1 = $R0;
-		my $lastdigit = $1;
-		if ($lastdigit < 2){
-		    $node = 0;
-		} elsif ($lastdigit < 4){
-		    $node = 1;
-		} elsif ($lastdigit < 6){
-		    $node = 2;
-		} elsif ($lastdigit < 8){
-		    $node = 3;
-		} else {
-		    die "Cannot determine NIC for $E0\n";
+	    if ($arch  eq "XC"){
+		# This endpoint is a NIC. aries has 4 NIC and 8 ptiles, in order. get the last digit
+		# NIC will be endpoint of 2 ptilelinks and 1 pcie link.
+		my $node = -1;
+		if ($E0 =~ /.*(\d)/){
+		    my $lname0 = "link" . $E0;
+		    $R1 = $R0;
+		    my $lastdigit = $1;
+		    if ($lastdigit < 2){
+			$node = 0;
+		    } elsif ($lastdigit < 4){
+			$node = 1;
+		    } elsif ($lastdigit < 6){
+			$node = 2;
+		    } elsif ($lastdigit < 8){
+			$node = 3;
+		    } else {
+			die "Cannot determine NIC for $E0\n";
+		    }
+		    
+		    $E1 = $R0 . "n". $node;
+#		    print "adding ptile <$E0>\n";
+		    addTile($R0,$E0,$arch);
+#		    print "adding endpint <$E0> to link $lname0\n";
+		    addEndpoint($E0,"TLE",$lname0); # add tile as endpoint
+		    addLink($R0,$E0,$R1,$E1,$lname0,$type); #add the ptile link. 
+		    addNICandNode($R1,$E1,$node,$lname0); # if DNE
 		}
-
-		$E1 = $R0 . "n". $node;
-		addAriesTile($R0,$E0);
-		addEndpoint($E0,"TLE",$lname0,"N/A");
-		addLink($R0,$E0,$R1,$E1,$lname0,$type); #add the ptile link
-		addAriesNIC($R1,$E1,$node,$lname0); #add the NIC, NODE, and pcie link
+	    } else {  # for gemini, the NIC facing tiles must be inferred
+		die "Can't handle processor tiles for Gemini";
 	    }
 	}
 
@@ -336,14 +397,38 @@ while(<>){
 	}
     }
 } # while
+close($fh);
 
-printPrefixes();
+if ($arch eq "XE"){
+# put ptiles in here
+}
+
+print "\@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n";
+print "\@prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .\n";
+print "\@prefix adms: <http://www.w3.org/ns/adms#> .\n";
+print "\@prefix dct: <http://purl.org/dc/terms/> .\n";
+print "\@prefix logset: <http://portal.nersc.gov/project/mpccc/sleak/resilience/datasets/logset#> .\n";
+print "\@prefix ddict: <http://portal.nersc.gov/project/mpccc/sleak/resilience/datasets/ddict#> .\n";
+print "\@prefix craydict: <http://FIXME/where/is/craydict/published#> .\n";
+print "\n";
+print "# declare myself and set a prefix:\n";
+print "\@base <http://FIXME/where/will/this/be/published> .\n";
+print "\@prefix : <FIXME-edison-arch#> .\n";
+print "\n";
+print ":\n";
+print "\ta adms:Asset ;\n";
+print "\tdct:title \"FIXME give this file a title\" ;\n";
+print "\trdfs:label \"FIXME short label\" ;\n";
+print "\t.\n";
+print "\n";
+
 printCabinet();
 printChassis();
-printSlot();
+printAriesSlot();
 printAries();
 printLink();
 printEndpoint();
 printPcieLink();
+
 
 
