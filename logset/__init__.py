afd9ef45b4f0a49ad31f87d5e3030727a1404749
Steve Leak <sleak@lbl.gov>
2019-09-13 14:14:32 -0700

    WIP: getting the setuptools install working

--- logset/__init__.py
+++ logset/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+name = "logset"
+
 from . import config
 from . import graph
 from . import queries
---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="logset",
+    version="0.1.0",
+    author="Steve Leak et al",
+    author_email="sleak@lbl.gov",
+    description="Utilities for dicovering and exploring HPC system logs",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/NERSC/LogSet",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    python_requires='>=3.6',
+    install_requires=[
+        'sqlalchemy',
+        'rdflib_sqlalchemy',
+        'rdflib',
+        # used by LocalNSM:
+        'bidict',
+        # handles configuration/settings
+        'toml',
+        'deepmerge',
+        # used in testing
+        'behave']
+)
+
